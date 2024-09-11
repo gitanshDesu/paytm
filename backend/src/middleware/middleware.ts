67e035e2d9e02d2b8d4417b33291a5bf36d837e7
Gitansh4 <manchandagitansh4@gmail.com>
2024-09-12 01:40:30 +0530

    added auth middleware

---
+++ backend/src/middleware/middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+// Extend the Request interface to include userId
+interface CustomRequest extends Request {
+    userId?: string; // Mark it as optional in case it's not always present
+  }
+export const authMiddleware = async (req:CustomRequest,res:Response,next:NextFunction)=>{
+    const {authHeader} = req.headers;
+    //Type Assertion used
+    if (!authHeader || !(authHeader as string).startsWith('Bearer ')) {
+        return res.status(403).json({});
+    }
+    //Type Assertion used
+    const token = (authHeader as string).split(" ")[1];
+    //Type Assertion used
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET!) as JwtPayload;
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(403).json({});
+    }
+}
