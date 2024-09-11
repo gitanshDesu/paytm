acb7fb9b579c6f6e950a528610f3aaa25123e00c
Gitansh4 <manchandagitansh4@gmail.com>
2024-09-11 21:51:49 +0530

    added cors,body-parser and jsonwebtoken libs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import rootRouter from '../src/routes/index'
+import rootRouter from '../src/routes/index';
+import cors from 'cors';
+import bodyParser = require('body-parser');
 dotenv.config();
 const app = express();
+// parse application/json
+app.use(bodyParser.json());
+//Enable All CORS Requests
+app.use(cors());
 //Route all request coming on /api/v1 to this rootRouter
 app.use("/api/v1",rootRouter);
 //connect to MongoDB URL
