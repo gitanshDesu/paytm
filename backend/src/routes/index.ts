e23c6636b34265219de0ba36003f9ae0190a6125
Gitansh4 <manchandagitansh4@gmail.com>
2024-09-12 22:25:34 +0530

    created new router for accounts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,12 @@
 //Creating routing file structure i.e. all file routes to /api/v1
 import express from 'express';
 import userRouter from './user'
+import accountRouter from './account';
 const router = express.Router();
 
 // Mounts all the routes in userRouter on /user
 //Mounts all the user routes in userRouter on api/v1/
-router.use("/user",userRouter)
+router.use("/user",userRouter);
+//Mount all the account routes in accountRouter on api/v1/
+router.use("/account",accountRouter);
 export default router;
