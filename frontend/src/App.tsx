801256b721ee783011ea2342b2128d8743f8125e
Gitansh4 <manchandagitansh4@gmail.com>
2024-09-15 17:16:47 +0530

    / redirectsto /signin when web app loads

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
 import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
 import Dashboard from "./pages/Dashboard"
@@ -11,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element = {<Navigate to = "/signin"/>}/>
       <Route path="/signup" element = {<Signup/>}/>
       <Route path="/signin" element = {<Signin/>}/>
       <Route path="/dashboard" element = {<Dashboard/>}/>
